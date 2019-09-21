4adacc67ff0385eca7cf09e52958af24a43470c1
Hanna Altunina <hanna.aljaxeeva@gmail.com>
2019-09-21 10:10:24 +0300

    созжает массив

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,31 @@
 'use strict';
 
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+var getRandomElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var getRandomArray = function (length, index) {
+  var arr = [];
+  for (var i = 0; i < length; i++) {
+    arr[i] = getRandomElement(index);
+  }
+  return arr;
+};
+
+var userName = {
+  name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
+  coatColor: getRandomElement(WIZARD_COAT_COLOR),
+  eyesColor: getRandomElement(WIZARD_EYES_COLOR)
+};
+
+var otherUsers = getRandomArray(4, userName);
+console.log(otherUsers);
+
